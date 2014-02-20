bb6be36a1c320573c6953abd068727e7200b65b9
mudzereli <mudz@charter.net>
2014-02-20 12:02:45 -0600

    update build process

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
-mkdir build\dayz_epoch
-xcopy /S /Y dayz_server\* build\dayz_epoch\*
-mkdir build\missions\epoch.chernarus
-xcopy /S /Y dayz_mission\* build\missions\epoch.chernarus\*
-xcopy /S /Y ..\antihack\* build\dayz_epoch\addons\antihack\*
+call %~dp0clean.bat
+xcopy /S /Y dayz_server\* build\vilayercodecustom\dayz_epoch\*
+xcopy /S /Y dayz_mission\* build\vilayercodecustom\missions\epoch.chernarus\*
+xcopy /S /Y ..\antihack\* build\vilayercodecustom\dayz_epoch\addons\antihack\*
